955f3e5227d5337c9962a8e59a1db55e91b1e89e
Hady Rashwan <h2rashwan@gmail.com>
2020-08-17 11:03:38 +0200

    working basic rest APIs

--- datamapper_setup.rb
+++ datamapper_setup.rb
@@ -2,7 +2,8 @@ require 'data_mapper'
 # require 'pry'
 
 env = ENV['RACK_ENV'] || "development"
-DataMapper.setup(:default, ENV['DATABASE_URL'] ||  "sqlite3::memory:")
+DataMapper.setup(:default, ENV['DATABASE_URL'] ||  "sqlite://#{Dir.pwd}/database.db")
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.auto_upgrade!
-DataMapper.finalize
+# DataMapper.auto_migrate!
+# DataMapper.auto_upgrade!
+# DataMapper.finalize
--- server.rb
+++ server.rb
@@ -9,9 +9,12 @@ class Todo
 
   property :id, Serial
   property :title, String
+  property :text, String
   property :done , Boolean
 
 end
+
+DataMapper.auto_upgrade!
 
 
 # class TodoExample < Sinatra::Application
@@ -20,18 +23,41 @@ end
     end
     
     get '/todo' do
-      'All todos'
+        Todo.all.to_json
     end
     
     post '/todo' do
-        'Item added'
+        body = JSON.parse( request.body.read.to_s )
+
+        todo = Todo.new(body)
+        if todo.save
+            redirect '/todo'
+        else
+            status 500
+            body {msg:"Can not add the item"}.to_json
+        end
     end
     
+    get '/todo/:id' do
+        Todo.get(params['id']).to_json
+    end
+
     put '/todo/:id' do
-        "Item with id:  #{params['id']} is updated"
+        todo = Todo.get(params['id'])
+        body = JSON.parse( request.body.read.to_s )
+        sucess =  todo.update(body)
+        if sucess
+            redirect "/todo/#{params['id']}"
+        else
+            status 500
+            body {msg:"Can not update the item"}.to_json
+        end
+
     end
     
     delete '/todo/:id' do
+        todo = Todo.get(params['id'])
+        todo.destroy!
         "Item with id: #{params['id']} is deleted"
     end
 
